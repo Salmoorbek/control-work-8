435ee7f5dc7b5ed1ae013d0853c8980bd09b7086
Salmoor Nurzhanov <salmor662@gmail.com>
2023-04-29 11:25:31 +0600

    init commit

---
+++ src/main/java/com/example/cw8/Cw8Application.java
@@ -0,0 +1,13 @@
+package com.example.cw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cw8Application.class, args);
+    }
+
+}
